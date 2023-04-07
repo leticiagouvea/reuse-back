0cd043f3df8ff3f0ed33c801160844a6d4cfdb0d
leticiagouvea <letfgouvea@gmail.com>
2023-04-07 12:44:59 -0300

    feat: sign-in route

---
+++ src/service/auth-service.ts
@@ -0,0 +1,48 @@
+import { requestError } from "@/errors/request-error";
+import sessionRepository from "@/repositories/session-repository";
+import userRepository from "@/repositories/user-repository";
+import { users } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export type SignInParams = Pick<users, "email" | "password">;
+
+const signIn = async (params: SignInParams) => {
+  const { email, password } = params;
+
+  const user = await userRepository.findByEmail(email, { 
+    id: true, 
+    email: true, 
+    password: true,
+    username: true,
+    city: true,
+    state: true,
+    cellphone: true
+  });
+
+  if (!user) throw requestError("E-mail or password are incorrect");
+
+  const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+  if (!isPasswordValid) throw requestError("E-mail or password are incorrect");
+
+  const token = jwt.sign({ user: user.id }, process.env.JWT_SECRET);
+
+  await sessionRepository.create({
+    token,
+    userId: user.id
+  });
+
+  delete user.password;
+
+  return {
+    user,
+    token
+  };
+};
+
+const authService = {
+  signIn,
+};
+
+export default authService;
